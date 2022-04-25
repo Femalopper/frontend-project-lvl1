0f35223b50ce28911ede116be269d91d648c8af9
Ангелина Головенко <lina-kursk@mail.ru>
2022-04-25 11:27:30 +0300

    add eslint.yml

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,10 @@
 import readlineSync from 'readline-sync';
-console.log("Welcome to the Brain Games!");
 
-export const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
+console.log('Welcome to the Brain Games!');
 
+const userName = () => {
+  const enterName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${enterName}!`);
+};
 
+export default userName;
